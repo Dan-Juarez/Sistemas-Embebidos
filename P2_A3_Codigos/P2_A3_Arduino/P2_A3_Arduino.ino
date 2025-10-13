8fa44717575538d3817f979ff90dadd976804e2b
Dan-Juarez <akamedurdo@gmail.com>
2025-10-13 09:43:34 -0600

    Actividad 3 Parcial 2

---
+++ P2_A3_Codigos/P2_A3_Arduino/P2_A3_Arduino.ino
@@ -0,0 +1,38 @@
+//Actividad P2 A3 Arduino con APP Bluetooth
+//Daniel Juárez || Viviana Morin
+
+//variables de las LEDS
+const byte Led1 = 2;
+const byte Led2 = 3;
+
+//Variable que recibe instruccion
+char orden;
+
+void setup() {
+  pinMode(Led1 , HIGH);
+  pinMode(Led2 , HIGH);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available()) {
+    orden = Serial.read();
+    if (orden == 'A')
+    {
+      digitalWrite(Led1, HIGH);
+    }
+    if (orden == 'B')
+    {
+      digitalWrite(Led1, LOW);
+    }
+    if (orden == 'C')
+    {
+      digitalWrite(Led2, HIGH);
+    }
+    if (orden == 'D')
+    {
+      digitalWrite(Led2, LOW);
+    }
+  } 
+}
